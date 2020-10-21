4dce5da35035a933ede0e86ae805de49a092cee1
louiscklaw <louiscklaw@gmail.com>
2020-10-21 20:14:23 +0800

    update,

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# cd utils
+#   sh clean.sh
+# cd ..
+
+# rm -rf /home/logic/_workspace/jobs_search_tryout/appliation_letter_queue/jobs_sc_assistant-it-officer-100003008017229
+
+# mv /home/logic/_workspace/jobs_search_tryout/ignore/jobs_sc_assistant-it-officer-100003008017229.png /home/logic/_workspace/jobs_search_tryout/screencapture/ | true
+
+
+rm -rf screencapture/*.*
+
+node src/index.js
