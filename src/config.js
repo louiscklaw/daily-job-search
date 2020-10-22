5663c6189a43ea6bdbbe400bc80985ed2647b3c4
louiscklaw <louiscklaw@gmail.com>
2020-10-23 01:43:39 +0800

    update and test,

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ const screencapture_path = `${PROJ_HOME}/screencapture`;
 // local test
 const local_test_ignore_sc_path = `${PROJ_HOME}/ignore`;
 const gh_ignore_sc_path = `${PROJ_HOME}/ignored_screen_capture`;
-const IGNORE_SC_PATH = gh_ignore_sc_path;
+const IGNORE_SC_PATH = ENV_PRODUCTION ? gh_ignore_sc_path : local_test_ignore_sc_path;
 
 const APPLICATION_LETTER_TEMPLATE_PATH = `${SRC_HOME}/application_letter_template`;
 const APPLICATION_LETTER_QUEUE_PATH=`${PROJ_HOME}/appliation_letter_queue`
