bbdd8187f858ed760a51bccdc978c702815d65be
louiscklaw <louiscklaw@gmail.com>
2020-10-22 17:18:40 +0800

    update,

--- src/fetchJobsDb.js
+++ src/fetchJobsDb.js
@@ -25,14 +25,14 @@ async function fetchJobsDb(jobsdb_fetch_config){
 }
 
 async function fetchJobsDbByCategoryAndKeywords(category,keywords) {
-  console.log(`getting list using ${category}, ${keywords}`)
-  const browser = await puppeteer.launch({
-    defaultViewport:{
-        width: 1920/2,
-        height: 5080
+  console.log( `getting list using ${category}, ${keywords}` )
+  const browser = await puppeteer.launch( {
+    defaultViewport: {
+      width: 1920 / 2,
+      height: 5080
     },
     ignoreHTTPSErrors: true
-  });
+  } );
   const page = await browser.newPage();
 
   await page.goto( `https://hk.jobsdb.com/hk/jobs/${category}/1?Key=${keywords}` );
@@ -41,14 +41,16 @@ async function fetchJobsDbByCategoryAndKeywords(category,keywords) {
   } );
 
   var page_content = await page.content()
-  fs.writeFileSync(`jobs_${keywords}_list.html`,page_content, {encoding:'utf-8'})
-  console.log(page_content.search(/href/g))
+  fs.writeFileSync( `jobs_${keywords}_list.html`, page_content, {
+    encoding: 'utf-8'
+  } )
+  console.log( page_content.search( /href/g ) )
 
   var test_in_html = page_content
-  var raw_job_links = test_in_html.match(/href="(\/hk\/en\/job\/.+?)"/g).sort();
-  var job_link_and_jobid = raw_job_links.map(x => {
-    var job_link =x.replace('href="','https://hk.jobsdb.com').replace('"','')
-    var job_id = x.match(/jobId=(\d+)/)[1]
+  var raw_job_links = test_in_html.match( /href="(\/hk\/en\/job\/.+?)"/g ).sort();
+  var job_link_and_jobid = raw_job_links.map( x => {
+    var job_link = x.replace( 'href="', 'https://hk.jobsdb.com' ).replace( '"', '' )
+    var job_id = x.match( /jobId=(\d+)/ )[ 1 ]
 
     var job_screencapture_filename = `jobs_sc_${getFilenameByJobLink(job_link)}.png`
 
@@ -58,16 +60,16 @@ async function fetchJobsDbByCategoryAndKeywords(category,keywords) {
       job_screencapture_filename
     }
 
-  })
+  } )
 
   // for (i=0;i<2;i++){
-  for (i=0;i<job_link_and_jobid.length;i++){
-    var active_job_link = job_link_and_jobid[i]
+  for ( i = 0; i < job_link_and_jobid.length; i++ ) {
+    var active_job_link = job_link_and_jobid[ i ]
 
     // console.log(`capture job ${active_job_link.job_link}`)
-    console.log(`saving screencapture ${active_job_link.job_screencapture_filename}`)
+    console.log( `saving screencapture ${active_job_link.job_screencapture_filename}` )
 
-    await page.goto(active_job_link.job_link)
+    await page.goto( active_job_link.job_link )
 
     await page.screenshot( {
       path: `${screencapture_path}/${active_job_link.job_screencapture_filename}`
