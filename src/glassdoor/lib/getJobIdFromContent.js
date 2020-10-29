9a9b6eafac8d4e7dc3bcbdba5cc09f4ba71b3825
louiscklaw <louiscklaw@gmail.com>
2020-10-30 02:09:45 +0800

    update,

--- src/glassdoor/lib/getJobIdFromContent.js
+++ src/glassdoor/lib/getJobIdFromContent.js
@@ -1,5 +1,11 @@
+const fs = require('fs')
+
 function getJobIdFromContent(page_content){
-  return page_content.match(/'id' : "(.+?)"/)[1]
+  try {
+    return page_content.match(/'id' : "(.+?)"/)[1]
+  } catch (error) {
+    fs.writeFileSync('./reading_id_fail_for_glassdoor.html',page_content,{encoding:'utf-8'})
+  }
 }
 
 module.exports={
